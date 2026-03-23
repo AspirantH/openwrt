af7fe84bd0191ba4015e745d4d28d0c095cc9c7e
learnscode <1645018@qq.com>
2026-03-23 21:02:00 +0800

    Update diy.sh

--- devices/common/diy.sh
+++ devices/common/diy.sh
@@ -83,7 +83,7 @@ sed -i \
 
 
 sed -i -e "s/set \${s}.country='\${country || ''}'/set \${s}.country='\${country || \"CN\"}'/g" -e "s/set \${s}.disabled=.*/set \${s}.disabled='0'/" package/network/config/wifi-scripts/files/lib/wifi/mac80211.uc
-
+sed -i 's/this.query == null && list.length == 0/false/g' feeds/luci/applications/luci-app-opkg/htdocs/luci-static/resources/view/system/opkg.js 2>/dev/null || true
 rm -rf package/feeds/packages/jool
 mkdir -p package/base-files/files/usr/bin
 cp -f diy/package/base-files/files/usr/bin/lucky package/base-files/files/usr/bin/lucky
