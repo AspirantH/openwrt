f938c7cfb055865e80b4a66850fa58becafad79b
AspirantH <93640658+AspirantH@users.noreply.github.com>
2026-09-06 16:59:38 +0800

    Update diy.sh

--- devices/common/diy.sh
+++ devices/common/diy.sh
@@ -5,10 +5,7 @@ shopt -s extglob
 sed -i '$a src-git AspirantH https://github.com/AspirantH/openwrt-packages.git;main' feeds.conf.default
 sed -i "/telephony/d" feeds.conf.default
 
-##sed -i "s?targets/%S/packages?targets/%S/\$(LINUX_VERSION)?" include/feeds.mk
-
 sed -i '/	refresh_config();/d' scripts/feeds
-
 sed -i "s?git.openwrt.org/\(project\|feed\)?github.com/openwrt?g" feeds.conf.default
 
 ./scripts/feeds update -a
@@ -50,7 +47,7 @@ sed -i "s/192.168.1/10.0.0/" package/base-files/files/bin/config_generate
 sed -i "s#false; \\\#true; \\\#" include/download.mk
 
 echo "$(date +"%s")" >version.date
-##sed -i '/$(curdir)\/compile:/c\$(curdir)/compile: package/opkg/host/compile' package/Makefile
+
 sed -i "s/DEFAULT_PACKAGES:=/DEFAULT_PACKAGES:=luci-app-firewall luci-app-package-manager \
 luci-base luci-compat kmod-nvme libcurl luci-lib-fs \
 wget-ssl curl autocore htop nano kmod-lib-zstd kmod-tcp-bbr kmod-tun ca-bundle ip-full ruby ruby-yaml unzip bash tar block-mount resolveip ds-lite swconfig luci-app-filemanager /" include/target.mk
@@ -60,21 +57,16 @@ sed -i "s/^.*vermagic$/\techo '1' > \$(LINUX_DIR)\/.vermagic/" include/kernel-de
 status=$(curl -H "Authorization: token $REPO_TOKEN" -s "https://api.github.com/repos/AspirantH/openwrt-packages/actions/runs" | jq -r '.workflow_runs[0].status')
 echo "$status"
 while [[ "$status" == "in_progress" || "$status" == "queued" ]];do
-	echo "wait 5s"
-	sleep 5
-	status=$(curl -H "Authorization: token $REPO_TOKEN" -s "https://api.github.com/repos/AspirantH/openwrt-packages/actions/runs" | jq -r '.workflow_runs[0].status')
+    echo "wait 5s"
+    sleep 5
+    status=$(curl -H "Authorization: token $REPO_TOKEN" -s "https://api.github.com/repos/AspirantH/openwrt-packages/actions/runs" | jq -r '.workflow_runs[0].status')
 done
 
 wget -N https://raw.githubusercontent.com/openwrt/packages/master/lang/golang/golang/Makefile -P feeds/packages/lang/golang/golang/
 
-#sed -i "/call Build\/check-size,\$\$(KERNEL_SIZE)/d" include/image.mk
-
 sed -i "/+= targz/d" include/image.mk
 
-
-# find target/linux/x86 -name "config*" -exec bash -c 'cat kernel.conf >> "{}"' \;
 sed -i 's/max_requests 3/max_requests 20/g' package/network/services/uhttpd/files/uhttpd.config
-#rm -rf ./feeds/packages/lang/{golang,node}
 sed -i "s/tty\(0\|1\)::askfirst/tty\1::respawn/g" target/linux/*/base-files/etc/inittab
 
 date=$(date +%Y%m%d)
@@ -83,13 +75,32 @@ sed -i -e "/\(# \)\?REVISION:=/c\REVISION:=$date" -e '/VERSION_CODE:=/c\VERSION_
 sed -i 's/option timeout 30/option timeout 60/g' package/system/rpcd/files/rpcd.config
 
 sed -i \
-	-e "s/+\(luci\|luci-ssl\|uhttpd\)\( \|$\)/\2/" \
-	-e "s/+nginx\( \|$\)/+nginx-ssl\1/" \
-	-e 's/+python\( \|$\)/+python3/' \
-	-e 's?../../lang?$(TOPDIR)/feeds/packages/lang?' \
-	package/feeds/AspirantH/*/Makefile
-
+    -e "s/+\(luci\|luci-ssl\|uhttpd\)\( \|$\)/\2/" \
+    -e "s/+nginx\( \|$\)/+nginx-ssl\1/" \
+    -e 's/+python\( \|$\)/+python3/' \
+    -e 's?../../lang?$(TOPDIR)/feeds/packages/lang?' \
+    package/feeds/AspirantH/*/Makefile
 
 sed -i -e "s/set \${s}.country='\${country || ''}'/set \${s}.country='\${country || \"CN\"}'/g" -e "s/set \${s}.disabled=.*/set \${s}.disabled='0'/" package/network/config/wifi-scripts/files/lib/wifi/mac80211.uc
+
 sed -i '/^apk =/,/--preserve-env/{ /--preserve-env/ { /--force-overwrite/! s/$/ \\\n--force-overwrite/; }; }' include/rootfs.mk
+
+
+LUCI_MOD_SYSTEM="feeds/luci/modules/luci-mod-system"
+if [ -d "$LUCI_MOD_SYSTEM" ]; then
+    rm -rf "$LUCI_MOD_SYSTEM/htdocs/luci-static/resources/view/system/leds.js"
+    rm -rf "$LUCI_MOD_SYSTEM/htdocs/luci-static/resources/view/system/led-trigger/"
+    sed -i '/"admin\/system\/leds": {/,/^	},/d' \
+        "$LUCI_MOD_SYSTEM/root/usr/share/luci/menu.d/luci-mod-system.json"
+    sed -i '/"fs": { "\/sys\/class\/leds": "directory" }/,/^	},/d' \
+        "$LUCI_MOD_SYSTEM/root/usr/share/luci/menu.d/luci-mod-system.json"
+    sed -i '/"init": "led",/d' \
+        "$LUCI_MOD_SYSTEM/root/usr/share/ucitrack/luci-mod-system-system.json"
+    sed -i 's/"getLEDs", //g' \
+        "$LUCI_MOD_SYSTEM/root/usr/share/rpcd/acl.d/luci-mod-system.json"
+else
+    echo "⚠️ Warning: $LUCI_MOD_SYSTEM not found, skip LED removal."
+fi
+
+sed -i '/AspirantH/d' "package/base-files/files/etc/apk/repositories.d/distfeeds.list"
 rm -rf package/feeds/packages/jool
